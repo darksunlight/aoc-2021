6c25bede8c5d83fe0048d8975a31447a83a3f297
darksunlight <darksunlight210@gmail.com>
2021-12-05 23:01:57 -0800

    :skull:

--- day6/golf2.js
+++ day6/golf2.js
@@ -1,1 +1,1 @@
-s=Array(9).fill(0);`${require('fs').readFileSync('t')}`.split`,`.map(x=>s[+x]++);for(i=0;i<256;i++)s=[...s.slice(1,7),s[0]+s[7],s[8],s[0]];console.log(s.reduce((p, c) => p + c, 0));
+s=Array(9).fill(0);`${require('fs').readFileSync('t')}`.split`,`.map(x=>s[+x]++);for(i=0;i<256;i++)s=[...s.slice(1,7),s[0]+s[7],s[8],s[0]];console.log(s.reduce((p,c)=>p+c,0));
