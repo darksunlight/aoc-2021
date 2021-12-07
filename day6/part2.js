9810be11cc6a82f96188b3a5a3a78c4b9ff94556
darksunlight <darksunlight210@gmail.com>
2021-12-06 19:19:16 -0800

    bigint

--- day6/part2.js
+++ day6/part2.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const input = fs.readFileSync(filename, 'utf-8').split(',').map(x => +x);
 console.log(`opening ${filename}`);
 
-let school = Array(9).fill(0);
+let school = Array(9).fill(0n);
 
 for (let i = 0; i < input.length; i++) {
     school[input[i]]++;
@@ -12,4 +12,4 @@ for (let i = 0; i < input.length; i++) {
 for (let i = 0; i < 256; i++) {
     school = [...[1, 2, 3, 4, 5, 6].map(x => school[x]), school[0] + school[7], school[8], school[0]];
 }
-console.log(school.reduce((p, c) => p + c, 0));
+console.log(school.reduce((p, c) => p + c, 0n));
